7278c2ea09b673c9deed6dcc3cdce9c66846dd6c
AlarmedEwe <guilherme11do9@gmail.com>
2021-11-18 22:58:05 -0300

    Reading JSON files + Organizing folder

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿string path = AppDomain.CurrentDomain.BaseDirectory + "/Languages";
+
+Console.WriteLine("+---------------------+");
+Console.WriteLine("|-- Select language --|");
+Console.WriteLine("|- en                -|");
+Console.WriteLine("|- pt-br             -|");
+Console.WriteLine("+---------------------+");
+
+string language = Console.ReadLine();
+
+string json = language switch
+{
+  "pt-br" => File.ReadAllText($"{path}/pt-br.json"),
+  "en" => File.ReadAllText($"{path}/en.json"),
+  _ => File.ReadAllText($"{path}/en.json")
+};
+
+Console.Clear();
